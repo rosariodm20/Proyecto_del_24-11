82354e7a5970363d67b4718cb9567f3ed0c84ba9
rosariodm20 <rosariodelgadomoreno17@gmail.com>
2023-11-24 19:26:04 +0100

    Subo proyecto con mi nombre

--- Viernes/src/ejercicio1.java
+++ Viernes/src/ejercicio1.java
@@ -4,6 +4,7 @@ public class ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola, soy Rosario");
+System.out.println("Estot en desarrollo");
 	}
 
 }
